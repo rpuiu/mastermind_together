6bc084a65ebaade9328b5e0f11e3aaba8337e9c7
rpuiu <raz.puiu@gmail.com>
2023-07-03 08:24:02 +0300

    Fixed deployment issue'

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ echo "Setting up permissions for SSH key..."
 chmod 600 ./id_rsa
 
 echo "Building Flutter app..."
+flutter clean
 flutter build web
 
 if [ $? -ne 0 ]; then
@@ -15,11 +16,22 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+echo "Deleting old files..."
+ssh -i ./id_rsa -o StrictHostKeyChecking=no root@$IP 'rm -rf /var/www/mmt/*'
+
 echo "Deploying to $HOST..."
-scp -i ./id_rsa -o StrictHostKeyChecking=no -r ./build/web $HOST
+scp -i ./id_rsa -o StrictHostKeyChecking=no -r ./build/web/* $HOST
 
 if [ $? -ne 0 ]; then
     echo "SCP failed"
+    exit 1
+fi
+
+echo "Restarting Nginx..."
+ssh -i ./id_rsa -o StrictHostKeyChecking=no root@$IP 'sudo service nginx restart'
+
+if [ $? -ne 0 ]; then
+    echo "Failed to restart Nginx"
     exit 1
 fi
 
