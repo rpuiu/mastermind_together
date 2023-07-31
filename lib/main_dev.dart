495a74d2647da3b844fea6274aa630448c540eb6
rpuiu <raz.puiu@gmail.com>
2023-07-31 15:12:33 +0300

    MAIN-T-9 : As Developer I want to have an error management system to have an overview of all the application issues

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mastermind_together/app.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
 import 'src/get_bindings.dart';
@@ -9,5 +12,24 @@ Future<void> main() async {
   await dotenv.load(fileName: ".env.dev");
   await GetBindings.init();
   tz.initializeTimeZones();
-  runApp(MyApp());
+
+  FlutterError.onError = (details, {bool forceReport = false}) {
+    Zone.current.handleUncaughtError(details.exception, details.stack!);
+  };
+
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = dotenv.env['SENTRY_DSN']; // Load the DSN from .env.dev
+      options.tracesSampleRate = 1.0;
+      options.debug = true; // Debug mode is enabled in development
+    },
+    appRunner: () => runZonedGuarded(() {
+      runApp(MyApp());
+    }, (error, stackTrace) {
+      Sentry.captureException(
+        error,
+        stackTrace: stackTrace,
+      );
+    }),
+  );
 }
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mastermind_together/app.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
 import 'src/get_bindings.dart';
@@ -9,5 +12,24 @@ Future<void> main() async {
   await dotenv.load(fileName: ".env.prod");
   await GetBindings.init();
   tz.initializeTimeZones();
-  runApp(MyApp());
+
+  FlutterError.onError = (details, {bool forceReport = false}) {
+    Zone.current.handleUncaughtError(details.exception, details.stack!);
+  };
+
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = dotenv.env['SENTRY_DSN']; // Load the DSN from .env.prod
+      options.tracesSampleRate = 1.0;
+      options.debug = false; // Debug mode is disabled in production
+    },
+    appRunner: () => runZonedGuarded(() {
+      runApp(MyApp());
+    }, (error, stackTrace) {
+      Sentry.captureException(
+        error,
+        stackTrace: stackTrace,
+      );
+    }),
+  );
 }
