1d7e475c965e4262f30546301623416a2370269e
rpuiu <raz.puiu@gmail.com>
2023-10-03 15:35:38 +0300

    MAIN-T-135 : As User I want to see only my main goal on the home screen

--- lib/src/home/sections/goals_section.dart
+++ lib/src/home/sections/goals_section.dart
@@ -4,19 +4,16 @@ import 'package:mastermind_together/src/goal/goal_card.dart';
 import 'package:mastermind_together/src/goal/goals_controller.dart';
 import 'package:mastermind_together/src/ui/theme/sizes.dart';
 import 'package:mastermind_together/src/ui/theme/text_styles.dart';
-import 'package:mastermind_together/src/ui/widgets/custom_page_indicator.dart';
 
 class GoalsSection extends GetView<GoalsController> {
   const GoalsSection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    double viewportFraction = MediaQuery.of(context).size.width > 600 ? 0.6 : 0.8; // Adjust this to make cards wider
-    final PageController pageController = PageController(viewportFraction: viewportFraction);
-
     return SizedBox(
       height: 325,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Align(
             alignment: Alignment.topLeft,
@@ -27,34 +24,17 @@ class GoalsSection extends GetView<GoalsController> {
           ),
           Expanded(
             child: Obx(
-              () => PageView.builder(
-                controller: pageController,
-                itemCount: controller.goals.length,
-                scrollDirection: Axis.horizontal,
-                pageSnapping: true,
-                itemBuilder: (_, index) {
-                  final goal = controller.goals[index];
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: fontSize / 2),
-                    child: GoalCard(goal: goal, index: index),
-                  );
-                },
-              ),
+              () {
+                final goal = controller.goals.isNotEmpty ? controller.goals[0] : null;
+                return goal != null
+                    ? Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: fontSize / 2),
+                        child: GoalCard(goal: goal, index: 0),
+                      )
+                    : const Center(child: Text('No goals available'));
+              },
             ),
           ),
-          xSpace,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 24,
-                child: CustomPageIndicator(
-                  pageController: pageController,
-                  itemCount: controller.goals.length,
-                ),
-              )
-            ],
-          )
         ],
       ),
     );
